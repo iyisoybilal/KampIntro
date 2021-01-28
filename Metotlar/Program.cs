b03d21d617af5e81af6b8a88db0b651d535791cd
bilaliyisoy <397213@ogr.ktu.edu.tr>
2021-01-28 19:07:21 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elmasi";
+
+            Urun urun1 = new Urun();
+
+            urun1.Adi = "Elma";
+            urun1.Fiyat = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyat = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            foreach  (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyat);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------------------");
+            }
+
+            Console.WriteLine("------------Metotlar---------------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            
+            
+        }
+    }
+}
